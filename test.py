9a612ce825bc1e6d88fb189c4341ced9feb724c2
stilist80 <spec2003@inbox.ru>
2016-05-17 01:07:07 +0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+!# /etc/python3
+
+''' I still couid not find possibility to highlight syntax in the file. Honestly I am on the stage of "ten fingers" type in English 
+learning and that text is typed without looking on the keyboard. I am pretty good in typing in Russain and think that saved month or even
+years from 2005 then I used to be ill for a few days and never remember why but installed the program named babytype and finished it in 
+couple of days. Next week working in office was very importand as I was making a lot of mistakes and as I remember wished to take a look
+on the keyboard very much. But the thing is that it took not a lot of time to become better. As I remember just a first few days - 10-20
+pages to finally confirm I could type as good and as fast as I could do it with looking on keyboard (I was already familiar with computers
+and erned money setting up operating systems and environment so I was already enough fast - the same like most of other people using 
+computers everyday) but the difference was in inner feeling. I removed "keyboard process" - it is something then you know where the each 
+key is but before each press you somehow observe keyboard managing it nor the screen. It usually when you learn "life typing" withour any
+system and then you could be good but as your fingers do not know where to be without general observing you have to spend time on it and 
+you will always have to check it. One-two-three-four - now it is thirteen lines typed in this file and I am slowly begin to get letters 
+faster then I remember where to move each of my fingers. That is the second or the third stage after you have finished any program which
+brings you the system and lead you through the the process when new letters will appear on the screen after first "asdf" and "jkl;". From
+line to line you type faster and still making mistakes you start automatically solving them also much faster. This speed comes not from
+your thinking but from your fingers which just start type what you think of. It could be said you forgetting about fingers and from line
+to line you feel they are just serving you.'''
